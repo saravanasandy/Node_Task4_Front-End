04507da5c72e0f2ab530f74e24e42cf902f09210
saravanasandy <msaravanakumar369@gmail.com>
2024-01-09 19:38:33 +0530

    new change

---
+++ src/config.js
@@ -0,0 +1,11 @@
+export const env = {
+    // api : "http://localhost:4000/auth",
+    // api1 : "http://localhost:4000/api",
+    // redirect : "http://localhost:4000/api/shortUrl"
+
+    api : "https://node-task4-back-end.vercel.app/auth",
+    api1 : "https://node-task4-back-end.vercel.app/api",
+    redirect : "https://node-task4-back-end.vercel.app/api/shortUrl"
+    
+    
+}
